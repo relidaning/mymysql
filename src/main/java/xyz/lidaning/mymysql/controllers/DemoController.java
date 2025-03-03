bf1ff9117c0af5be37d0528aaa8cd877f0a93340
lidaning <41282656+relidaning@users.noreply.github.com>
2025-03-03 22:58:42 +0800

    init tables and datas

---
+++ src/main/java/xyz/lidaning/mymysql/controllers/DemoController.java
@@ -0,0 +1,58 @@
+package xyz.lidaning.mymysql.controllers;
+
+import java.lang.reflect.InvocationTargetException;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+import xyz.lidaning.common.JsonResult;
+import xyz.lidaning.mymysql.domains.User;
+import xyz.lidaning.random.RandomObjectUtil;
+@Slf4j
+@RestController
+public class DemoController {
+
+  @Autowired
+  JdbcTemplate jdbcTemplate;
+
+  @Transactional
+  @PostMapping("/initTables")
+  public JsonResult initTables(){
+    jdbcTemplate.execute("DROP TABLE IF EXISTS users");
+    jdbcTemplate.execute("CREATE TABLE users (\n" +
+      "  id INT AUTO_INCREMENT PRIMARY KEY,\n" +
+      "  username VARCHAR(100) NOT NULL,\n" +
+      "  gender VARCHAR(2)," +
+      "  birthday date ,\n" +
+      "  age INT NOT NULL\n" +
+      ")  ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ;");
+    return JsonResult.success();
+  }
+
+  @PostMapping("/insert1millionUsers")
+  public JsonResult insert1millionUsers() throws Exception{
+    List<User> users = new ArrayList<>();
+    for (int i = 0; i < 10000000; i++) {
+      users.add(RandomObjectUtil.randomInstance(User.class));
+    }
+    jdbcTemplate.batchUpdate("INSERT INTO users (username, gender, birthday, age) " +
+        "VALUES (?, ?, ?, ?)",
+        users,
+        1000,
+        (PreparedStatement ps, User u) -> {
+          log.debug("user: {}", u);
+          ps.setString(1, u.getUsername());
+          ps.setString(2, u.getGender());
+          ps.setDate(3, new java.sql.Date(u.getBirthday().getTime()));
+          ps.setInt(4, u.getAge());
+        });
+    return JsonResult.success();
+  }
+}
---
+++ src/main/java/xyz/lidaning/mymysql/domains/User.java
@@ -0,0 +1,40 @@
+package xyz.lidaning.mymysql.domains;
+
+import java.beans.Transient;
+import java.math.BigDecimal;
+import java.util.Date;
+
+import lombok.Data;
+import xyz.lidaning.random.Random;
+
+@Data
+public class User {
+  private int id;
+  @Random({"张三","李四","王五","赵六","钱七","孙八","周九","吴十"})
+  private String username;
+  private transient String password;
+  @Random({"0","1"}) 
+  private String gender;
+  @Random(dates={"1990-01-01","1991-01-01","1992-01-01","1993-01-01","1994-01-01",
+    "1995-01-01","1996-01-01","1997-01-01","1998-01-01","1999-01-01"})
+  private Date birthday;
+  @Random(integers = {18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35})
+  private int age;
+  @Random({"18512345678","18512345679","18512345680","18512345681","18512345682",
+    "18512345683","18512345684","18512345685"})
+  private String phone;
+  @Random({"18512345678@qq.com", "18512345679@qq.com", "18512345680@qq.com", 
+    "18512345681@qq.com", "18512345682@qq.com", "18512345683@qq.com", "18512345684@qq.com"})
+  private String email;
+  private BigDecimal balance;
+  
+  private String nativePalce;
+  private String residentPlace;
+  
+  private String degree;
+  
+  private String job;
+  
+  private String hobby;
+
+}
