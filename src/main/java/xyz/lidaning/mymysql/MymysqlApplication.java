4c354c7bd6c2542fe95bf9eb5e9bd61c5dbd299d
lidaning <41282656+relidaning@users.noreply.github.com>
2025-03-03 15:47:12 +0800

    init project

---
+++ src/main/java/xyz/lidaning/mymysql/MymysqlApplication.java
@@ -0,0 +1,13 @@
+package xyz.lidaning.mymysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/lidaning/mymysql/MymysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.lidaning.mymysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
